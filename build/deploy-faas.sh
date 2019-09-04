7aef8223f4fc8c5549befa720e3c5b45e63b966f
barret <47966933+Barretem@users.noreply.github.com>
2019-09-04 19:23:51 +0800

    fix: 更改构建信息

--- build/deploy-faas.sh
+++ build/deploy-faas.sh
@@ -15,5 +15,5 @@ cp -rf ../../examples/element-ui/** $SUB_FOLDER/
 cd ../..
 
 # deploy domestic site
-# faas deploy alpha -P element
-# rm -rf temp_web
+faas deploy alpha -P element
+rm -rf temp_web
