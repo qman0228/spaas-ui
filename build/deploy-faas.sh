9c840eb1cdf6b5aaa9f1d09874f1a7929d1eb76e
barret <47966933+Barretem@users.noreply.github.com>
2019-09-04 11:59:59 +0800

    feat: 调整正式发版build流程

--- build/deploy-faas.sh
+++ build/deploy-faas.sh
@@ -3,7 +3,7 @@ set -ex
 mkdir temp_web
 npm run deploy:build
 cd temp_web
-git clone --depth 1 -b gh-pages --single-branch https://github.com/ElemeFE/element.git && cd element
+git clone --depth 1 -b gh-pages --single-branch https://github.com/spaasteam/spaas-ui.git && cd spaas-ui
 
 # build sub folder
 SUB_FOLDER='2.12'
@@ -15,5 +15,5 @@ cp -rf ../../examples/element-ui/** $SUB_FOLDER/
 cd ../..
 
 # deploy domestic site
-faas deploy alpha -P element
-rm -rf temp_web
+# faas deploy alpha -P element
+# rm -rf temp_web
