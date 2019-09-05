763a67e57a9f996b169407236916ca18453ca11e
barret <47966933+Barretem@users.noreply.github.com>
2019-09-05 21:29:52 +0800

    feat: 调整发版流程

--- build/pub.sh
+++ build/pub.sh
@@ -2,15 +2,16 @@
 # 设置全局变量
 chmod 777 ./build/*.sh
 source ./build/env.sh
-# git-release
-GREN_GITHUB_TOKEN=$GITHUB_TOKEN
-gren release --override
 
 # release
 ./build/release.sh
 
 # algolia
 node build/bin/gen-indices.js
+
+# git-release
+GREN_GITHUB_TOKEN=$GITHUB_TOKEN
+gren release --override
 
 # 通过订单进行通知
 ./build/notify.sh
